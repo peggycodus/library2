4731b1716dc94451bba8f6b95c1955a555f31079
Aoede Pando & Peggy Condon <student@epicodus.com>
2014-08-12 09:53:50 -0700

    start books class with an empty array

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,14 @@
 class Book
   attr_reader :title, :author
+  @@all_books = []
 
   def initialize(attributes)
     @title = attributes['title']
     @author = attributes['author']
   end
 
+  def Book.all
+     @@all_books
+   end
 end
 
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -5,5 +5,15 @@ describe 'Book' do
     test_book = Book.new({'title' => 'Mrs. Dalloway', 'author' => 'Virginia Woolf'})
     expect(test_book).to be_an_instance_of Book
   end
+
+  it 'starts with an empty array' do
+    test_book = Book.new({'title' => 'Mrs. Dalloway', 'author' => 'Virginia Woolf'})
+    expect(Book.all).to eq []
+  end
+
+  # it 'saves a book to the array of books' do
+  #   test_book = Book.new({'title' => 'Mrs. Dalloway', 'author' => 'Virginia Woolf'})
+  #   test_book.save
+  #   expect(Book.all).to eq
 end
 
